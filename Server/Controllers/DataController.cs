5f90eb490e677cfe480337382c7f1d0a481ff2b4
NULL <NULL@HOME-PC>
2023-10-06 15:23:18 +0300

    Excel importer

--- Server/Controllers/DataController.cs
+++ Server/Controllers/DataController.cs
@@ -1,7 +1,12 @@
 ﻿using EcoMonitor.Shared;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using static System.Net.WebRequestMethods;
+using System.Text;
 
 namespace EcoMonitor.Server.Controllers
 {
@@ -9,13 +14,6 @@ namespace EcoMonitor.Server.Controllers
     [Route("api/pollution")]
     public class DataController : ControllerBase
     {
-        public class UpdateRecordModel
-        {
-            public int Id { get; set; }
-            public float Value { get; set; }
-            public int Year { get; set; }
-        }
-
         public DataController()
         {
             
@@ -52,7 +50,7 @@ namespace EcoMonitor.Server.Controllers
 
 
         [HttpPost("updaterecord")]
-        public IActionResult PostUpdate([FromBody] UpdateRecordModel model)
+        public IActionResult PostUpdate([FromBody] PostMessageUpdateRecord model)
         {
             var record = _context.Records.Find(model.Id);
             if (record == null)
@@ -65,6 +63,65 @@ namespace EcoMonitor.Server.Controllers
             return Ok();
         }
 
+
+        [HttpPost("addrecord")]
+        public IActionResult PostAddRecord([FromBody] PostMessageAddRecord model)
+        {
+            _context.Records.Add(new Record() { ObjectId = model.ObjectId, PollutantId = model.PollutantId, PollutionValue = model.Value, RecordYear = model.Year});
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost("uploadImport")]
+        public async Task Save(IList<IFormFile> chunkFile, IList<IFormFile> UploadFiles)
+        {
+            try
+            {
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                foreach (var file in UploadFiles)
+                {
+                    using (var stream = file.OpenReadStream())
+                    {
+                        using (var reader = ExcelReaderFactory.CreateReader(stream, new ExcelReaderConfiguration() { FallbackEncoding = Encoding.UTF8 }))
+                        {
+                            string factory = null;
+                            int factoryId = -1;
+                            while (reader.Read())
+                            {
+                                string temp = reader.GetString(0);
+                                if (temp != null)
+                                {
+                                    factory = temp;
+                                    var factoryObject = _context.Object.FirstOrDefault(x => x.ObjectName == factory);
+                                    if (factoryObject != null)
+                                        factoryId = factoryObject.Id;
+                                    else
+                                        throw new KeyNotFoundException($"Factory {factory} not found");
+                                }
+                                string pollutant = reader.GetString(1);
+                                double pollution = reader.GetDouble(2);
+                                int pollutantId = -1;
+                                var pollutantObject = _context.Pollutant.FirstOrDefault(x => x.PollutantName == pollutant);
+                                if (pollutantObject != null)
+                                    pollutantId = pollutantObject.Id;
+                                else
+                                    throw new KeyNotFoundException($"Pollutant {pollutant} not found");
+
+                                if (pollutantId != -1)
+                                {
+                                    _context.Records.Add(new Record() { ObjectId = factoryId, PollutantId = pollutantId, PollutionValue = (float)pollution, RecordYear = 2025 });
+                                    _context.SaveChanges();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
 
         private DatabaseContext _context = new DatabaseContext();
---
+++ Shared/PostMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoMonitor.Shared
+{
+    public class PostMessageUpdateRecord
+    {
+        public int Id { get; set; }
+        public float Value { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class PostMessageAddRecord
+    {
+        public int ObjectId { get; set; }
+        public int PollutantId { get; set; }
+        public float Value { get; set; }
+        public int Year { get; set; }
+    }
+}
